875c21cca8b06998be0ecbfc40677fe9d8a21e9b
Kebs <leymoevanzor@gmail.com>
2026-05-19 19:29:45 +0300

    Structure

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ load_dotenv()
 api_key = os.getenv("BOT_TOKEN")
 print(f"Ключ загружен: {api_key[:4]}...")
 
+bot = telebot.TeleBot(api_key)
 
-    
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Салам пополам!')
+
+
+
+if __name__ == "__main__":
+    print('Ботик запущен')
+    bot.polling(non_stop=True)
