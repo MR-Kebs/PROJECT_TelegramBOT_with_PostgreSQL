d065f1546b0ead79ce2f4dd615656bf288d5b015
kosayk06 <magomedkasiev@gmail.com>
2026-05-19 22:47:11 +0300

    Create two keybords, and start work  add_day route

--- keyboards.py
+++ keyboards.py
@@ -3,9 +3,36 @@ from telebot.types import (
     ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 )
 
-def start():
+def menu():
     markup = InlineKeyboardMarkup()
     markup.add(
-        InlineKeyboardButton("Начать", callback_data="start")
+        InlineKeyboardButton("➕ Записать", callback_data="add_day"),
+        InlineKeyboardButton("Статистика", callback_data="stats"),
+        InlineKeyboardButton("История", callback_data="history"),
+        InlineKeyboardButton("Настройки", callback_data="settings"),
+        InlineKeyboardButton("Помощь", callback_data="help"),
+        InlineKeyboardButton("Очистить данные", callback_data="clear_data")
+    )
+    return markup
+
+def mood():
+    markup = InlineKeyboardMarkup()
+    markup.add(
+        InlineKeyboardButton("1 - 😞", callback_data="1"),
+        InlineKeyboardButton("2 - 😐", callback_data="2"),
+        InlineKeyboardButton("3 - 🙂", callback_data="3"),
+        InlineKeyboardButton("4 - 😊", callback_data="4"),
+        InlineKeyboardButton("5 - 🤩", callback_data="5")
+    )
+    return markup
+
+def stats():
+    markup = InlineKeyboardMarkup()
+    markup.add(
+        InlineKeyboardButton("📅 За неделю", callback_data="week"),
+        InlineKeyboardButton("📅 За месяц", callback_data="month"),
+        InlineKeyboardButton("🔍 Мои инсайты", callback_data="insights"),
+        InlineKeyboardButton("📉 График", callback_data="graphs"),
+        InlineKeyboardButton("🔙 Назад", callback_data="back")
     )
     return markup
--- main.py
+++ main.py
@@ -14,7 +14,31 @@ bot = telebot.TeleBot(api_key)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Салам пополам!')
+    bot.send_message(message.chat.id, 'Добро пожаловать в меню!\nНажмите "+" для добавления дня.')
+    bot.send_message(message.chat.id, 'Выберите действие:', reply_markup=keyboards.menu())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "add_day")
+def add_day(call):
+
+    data = {
+        "user_id": call.from_user.id,
+        'mood': None,
+        'work_hours': None,
+        'sleep_hours': None,
+        'commit': None
+    }
+    bot.send_message(call.message.chat.id, 'Выберите ваше настроение от 1😞 до 5🤩:', reply_markup=keyboards.mood())
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_all_callbacks(call):
+    user_id = call.from_user.id
+    
+    if user_states.get(user_id) == "waiting_mood":
+        pass
+
 
 
 if __name__ == "__main__":
