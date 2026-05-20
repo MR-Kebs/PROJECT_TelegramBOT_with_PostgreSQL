6f91eb9211b059849c5acce33d037a636354dbd1
Kebs <leymoevanzor@gmail.com>
2026-05-20 20:53:56 +0300

    Split

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
+from datetime import datetime, time
 from database import get_users_to_remind
 
 scheduler = BackgroundScheduler()
 
 def send_reminders(bot):
     current_time = datetime.now().strftime("%H:%M")
+    fix_time = time()
     users = get_users_to_remind(current_time)
     for user in users:
-        bot.send_message(user[0], "Самое время отметиться!")
+        if current_time 
+            bot.send_message(user[0], "Самое время отметиться!")
 
 
 def start_scheduler(bot):
