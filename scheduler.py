ad0356a02a90c0c1528996a7a8e1800681c96f74
Kebs <leymoevanzor@gmail.com>
2026-05-20 19:34:55 +0300

    Automatic reminder system

---
+++ scheduler.py
@@ -0,0 +1,16 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from database import get_users_to_remind
+
+scheduler = BackgroundScheduler()
+
+def send_reminders(bot):
+    current_time = datetime.now().strftime("%H:%M")
+    users = get_users_to_remind(current_time)
+    for user in users:
+        bot.send_message(user, "Самое время отметиться!")
+
+
+def start_scheduler(bot):
+    scheduler.add_job(send_reminders, 'interval', minutes = 1, args=[bot])
+    scheduler.start()
