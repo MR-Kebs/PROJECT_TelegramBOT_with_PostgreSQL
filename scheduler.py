fc796336c1ac4272db1798b99cfcdff2daa8dbf0
Kebs <leymoevanzor@gmail.com>
2026-05-20 21:14:54 +0300

    Vide and Atmosphere system

--- scheduler.py
+++ scheduler.py
@@ -7,18 +7,18 @@ scheduler = BackgroundScheduler()
 active = False
 
 def send_reminders(bot):
-    if active == False:
-        return
     current_time = datetime.now().strftime("%H:%M")
-    fix_time = time()
+    ct = datetime.now().time()
+    start_vibe = time(4, 00)
+    start_atmosphere = time(16, 00)
     users = get_users_to_remind(current_time)
     for user in users:
-        if current_time 
-            bot.send_message(user[0], "Самое время отметиться!")
+        if start_vibe <= ct < start_atmosphere:
+            bot.send_message(user[0], "Самое время отметить свой вайбик!")
+        else:
+            bot.send_message(user[0], "Самое время отметить свою атмосферу!")
 
 
 def start_scheduler(bot):
-    if active == False:
-        return
     scheduler.add_job(send_reminders, 'interval', minutes = 1, args=[bot])
     scheduler.start()
